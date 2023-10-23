97fc3e4a8caeaae816caeeb791e922e40065e3b9
kwangjin <kwangjin5468@naver.com>
2023-10-24 00:13:19 +0900

    10/24 board create table, 데이터 insert 까지 구현

--- src/main/java/com/minipro/springweb/repository/BoardRepository.java
+++ src/main/java/com/minipro/springweb/repository/BoardRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-// Repository는 Entity클래스만 받아준다.(관례)
+// Repository는 Entity클래스만 받아준다.
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 }
